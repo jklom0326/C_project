7d96ed957cacdacafb6e3863f0a27bb1116df719
Sangwan Han <jklom0326@gmail.com>
2021-09-16 19:13:06 +0900

    c

---
+++ exercise/input_output/exer03.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    int a,b;
+    printf("가로 길이를 입력하세요 : ");
+    scanf("%d",&a);
+    printf("세로 길이를 입력하세요 : ");
+    scanf("%d",&b);
+    printf("직사각형의 넓이는 %d입니다. \n", a*b);
+}
