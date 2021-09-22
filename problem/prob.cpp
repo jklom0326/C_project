6877f9afa0f2d0eba0e025daa751f5a748a61d42
Sangwan Han <jklom0326@gmail.com>
2021-09-22 21:20:59 +0900

    c

---
+++ problem/prob.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+    int num = 0;
+    while (num < 5)
+    {
+        printf("반복내용 : %d \n", num);
+        num++;
+    }
+
+    printf("반복문을 종료한 후 %d \n", num);
+    
+}
