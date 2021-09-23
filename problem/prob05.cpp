8b6bb5774aeaa1adba4c1064457d977a6ea1bb60
Sangwan Han <jklom0326@gmail.com>
2021-09-23 23:02:40 +0900

    c

---
+++ problem/prob05.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int i = 0, sum =0;
+
+    while (i <= 10)
+    {
+        sum = sum + i;
+        printf("i = %d, sum = %d \n", i,sum);
+        i++;
+    }
+    printf("--------------반복문 종료------------");    
+}
